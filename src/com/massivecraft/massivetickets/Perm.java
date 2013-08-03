90a915916a2d99a77f2ffaba24e658bbbe96a23f
Olof Larsson <olof@sylt.nu>
2013-08-03 10:39:40 +0200

    Add in a version perm and the Perm enum.

---
+++ src/com/massivecraft/massivetickets/Perm.java
@@ -0,0 +1,59 @@
+package com.massivecraft.massivetickets;
+
+import org.bukkit.permissions.Permissible;
+
+import com.massivecraft.mcore.util.PermUtil;
+
+public enum Perm
+{
+	// -------------------------------------------- //
+	// ENUM
+	// -------------------------------------------- //
+	
+	LIST("list"),
+	SHOW("show"),
+	SHOW_OTHER("show.other"),
+	CREATE("create"),
+	DONE("done"),
+	DONE_OTHER("done.other"),
+	PICK("pick"),
+	YIELD("yield"),
+	YIELD_OTHER("yield.other"),
+	HIGHSCORE("highscore"),
+	MODLIST("modlist"),
+	WORKING("working"),
+	VERSION("version"),
+	
+	// END OF LIST
+	;
+	
+	// -------------------------------------------- //
+	// FIELDS
+	// -------------------------------------------- //
+	
+	public final String node;
+	
+	// -------------------------------------------- //
+	// CONSTRUCT
+	// -------------------------------------------- //
+	
+	Perm(final String node)
+	{
+		this.node = "massivetickets."+node;
+	}
+	
+	// -------------------------------------------- //
+	// HAS
+	// -------------------------------------------- //
+	
+	public boolean has(Permissible permissible, boolean informSenderIfNot)
+	{
+		return PermUtil.has(permissible, this.node, informSenderIfNot);
+	}
+	
+	public boolean has(Permissible permissible)
+	{
+		return has(permissible, false);
+	}
+	
+}
