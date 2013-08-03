21aa1ac844b48916851cc498075d2b8698529ab2
Olof Larsson <olof@sylt.nu>
2013-08-03 18:32:06 +0200

    Implemented the working command

--- src/com/massivecraft/massivetickets/cmd/CmdTicketsWorking.java
+++ src/com/massivecraft/massivetickets/cmd/CmdTicketsWorking.java
@@ -2,7 +2,10 @@ package com.massivecraft.massivetickets.cmd;
 
 import java.util.List;
 
+import com.massivecraft.massivetickets.MassiveTickets;
 import com.massivecraft.massivetickets.Perm;
+import com.massivecraft.massivetickets.entity.MPlayerColl;
+import com.massivecraft.mcore.cmd.arg.ARBoolean;
 import com.massivecraft.mcore.cmd.req.ReqHasPerm;
 
 public class CmdTicketsWorking extends MassiveTicketsCommand
@@ -19,6 +22,31 @@ public class CmdTicketsWorking extends MassiveTicketsCommand
 	@Override
 	public void perform()
 	{
-		// TODO
+		// Args
+		boolean old = msender.isWorking();
+		Boolean target = this.arg(0, ARBoolean.get(), !old);
+		if (target == null) return;
+		
+		// Detect Nochange
+		if (old == target)
+		{
+			if (target)
+			{
+				msg("<i>You are already working.");
+			}
+			else
+			{
+				msg("<i>You are already not working.");
+			}
+			return;
+		}
+		
+		// Apply
+		msender.setWorking(target);
+		
+		// Inform
+		String verb = target ? "started" : "stopped"; 
+		MassiveTickets.alertMsg("<white>%s <pink>%s working. There's now <aqua>%d <pink>moderators.", msender.getDisplayName(), verb, MPlayerColl.get().getAllCurrentlyWorking().size());
 	}
+	
 }
