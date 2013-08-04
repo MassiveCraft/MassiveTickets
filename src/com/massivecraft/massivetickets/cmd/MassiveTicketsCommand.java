4e12da0f406b077291e42efe8154463fe9244e6e
Olof Larsson <olof@sylt.nu>
2013-08-04 02:41:33 +0200

    Commands should be secret to not confuse non moderators.

--- src/com/massivecraft/massivetickets/cmd/MassiveTicketsCommand.java
+++ src/com/massivecraft/massivetickets/cmd/MassiveTicketsCommand.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.massivecraft.massivetickets.entity.MPlayer;
 import com.massivecraft.mcore.cmd.MCommand;
+import com.massivecraft.mcore.cmd.VisibilityMode;
 
 public abstract class MassiveTicketsCommand extends MCommand
 {
@@ -18,6 +19,7 @@ public abstract class MassiveTicketsCommand extends MCommand
 	public MassiveTicketsCommand(List<String> aliases)
 	{
 		this.setAliases(aliases);
+		this.setVisibilityMode(VisibilityMode.SECRET);
 	}
 	
 }
