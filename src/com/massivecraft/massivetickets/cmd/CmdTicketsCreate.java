8df6495f575cfc5116590ebda111b882902f02d2
Olof Larsson <olof@sylt.nu>
2013-08-04 11:16:48 +0200

    The command suggestions should not look better.

--- src/com/massivecraft/massivetickets/cmd/CmdTicketsCreate.java
+++ src/com/massivecraft/massivetickets/cmd/CmdTicketsCreate.java
@@ -7,6 +7,7 @@ import org.bukkit.ChatColor;
 import com.massivecraft.massivetickets.MassiveTickets;
 import com.massivecraft.massivetickets.Perm;
 import com.massivecraft.massivetickets.entity.MPlayerColl;
+import com.massivecraft.mcore.cmd.MCommand;
 import com.massivecraft.mcore.cmd.arg.ARString;
 import com.massivecraft.mcore.cmd.req.ReqHasPerm;
 
@@ -42,9 +43,18 @@ public class CmdTicketsCreate extends MassiveTicketsCommand
 		MassiveTickets.alertMsg(sender, "Your ticket was %s. We will help you asap.", verb);
 		MassiveTickets.alertMsg(sender, "There is currently <aqua>%d <pink>working moderators.", MPlayerColl.get().getAllCurrentlyWorking().size());
 		
-		MassiveTickets.alertMsg(sender, "Use " + MassiveTickets.get().getOuterCmdTickets().cmdTicketsShow.getUseageTemplate() + " <pink>to show your ticket");
-		MassiveTickets.alertMsg(sender, "Use " + MassiveTickets.get().getOuterCmdTickets().cmdTicketsDone.getUseageTemplate() + " <pink>to mark it as done");
-		MassiveTickets.alertMsg(sender, "Use " + MassiveTickets.get().getOuterCmdTickets().cmdTicketsModlist.getUseageTemplate() + " <pink>to list the moderators");
-		MassiveTickets.alertMsg(sender, "Use " + MassiveTickets.get().getOuterCmdTickets().cmdTicketsCreate.getUseageTemplate() + " <pink>to update your current ticket message");
+		MCommand cmd = null;
+		
+		cmd = MassiveTickets.get().getOuterCmdTickets().cmdTicketsShow;
+		MassiveTickets.alertMsg(sender, "Use " + cmd.getUseageTemplate(cmd.getCommandChain(), false, true, sender) + " <pink>to show your ticket");
+		
+		cmd = MassiveTickets.get().getOuterCmdTickets().cmdTicketsDone;
+		MassiveTickets.alertMsg(sender, "Use " + cmd.getUseageTemplate(cmd.getCommandChain(), false, true, sender) + " <pink>to mark it as done");
+		
+		cmd = MassiveTickets.get().getOuterCmdTickets().cmdTicketsModlist;
+		MassiveTickets.alertMsg(sender, "Use " + cmd.getUseageTemplate(cmd.getCommandChain(), false, true, sender) + " <pink>to list the moderators");
+		
+		cmd = MassiveTickets.get().getOuterCmdTickets().cmdTicketsCreate;
+		MassiveTickets.alertMsg(sender, "Use " + cmd.getUseageTemplate(cmd.getCommandChain(), false, true, sender) + " <pink>to update your current ticket message");
 	}
 }
