a207f966733e832670df0fd42e45dffd10be7214
Olof Larsson <olof@sylt.nu>
2014-09-12 10:22:05 +0200

    MassiveCore change.

--- src/com/massivecraft/massivetickets/cmd/MassiveTicketsCommand.java
+++ src/com/massivecraft/massivetickets/cmd/MassiveTicketsCommand.java
@@ -14,6 +14,12 @@ public abstract class MassiveTicketsCommand extends MassiveCommand
 		this.msender = MPlayer.get(sender);
 	}
 	
+	@Override
+	public void unsetSenderVars()
+	{
+		this.msender = null;
+	}
+	
 	public MassiveTicketsCommand()
 	{
 		this.setVisibilityMode(VisibilityMode.SECRET);
