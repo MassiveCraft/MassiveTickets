bb906f65502db5891c7d02ff5c16370c51db34ab
Olof Larsson <olof@sylt.nu>
2015-07-24 13:44:36 +0200

    MassiveCore Change

--- src/com/massivecraft/massivetickets/entity/ARMPlayer.java
+++ src/com/massivecraft/massivetickets/entity/ARMPlayer.java
@@ -1,5 +1,6 @@
 package com.massivecraft.massivetickets.entity;
 
+import com.massivecraft.massivecore.SenderPresence;
 import com.massivecraft.massivecore.cmd.arg.AR;
 
 public class ARMPlayer
@@ -11,7 +12,7 @@ public class ARMPlayer
 	private static AR<MPlayer> any = MPlayerColl.get().getAREntity();
 	public static AR<MPlayer> getAny() { return any; }
 	
-	private static AR<MPlayer> online = MPlayerColl.get().getAREntity(true);
+	private static AR<MPlayer> online = MPlayerColl.get().getAREntity(SenderPresence.ONLINE);
 	public static AR<MPlayer> getOnline() { return online; }
 	
 }
